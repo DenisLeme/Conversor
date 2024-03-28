98b6a1e082d6be883c1e07b2d5683c8430340a77
Denis Leme <denis_leme@icloud.com>
2024-03-28 08:02:14 -0300

    feat: first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,90 @@
-import Image from "next/image";
+"use client"
+import React, { useState, useEffect } from 'react';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const [selectedCurrency, setSelectedCurrency] = useState("US$ Dolar americano");
+  const [inputValue, setInputValue] = useState("");
+  const [realValue, setRealValue] = useState("");
+  const [convertedValue, setConvertedValue] = useState("");
+
+  const currencies = [
+    { name: "US$ Dolar americano", imgSrc: "/convert/assets/estados-unidos.png" },
+    { name: "€ Euro", imgSrc: "/convert/assets/euro.png" },
+    { name: "BitCoin", imgSrc: "/convert/assets/bitCoin.png" }
+  ];
+  
+
+  useEffect(() => {
+    convertValues();
+  }, [inputValue, selectedCurrency]); // Executar a conversão sempre que o valor de entrada ou a moeda selecionada mudar
+
+  const convertValues = async () => {
+    // Simulação de chamada à API
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json());
+
+    const dolar = data.USDBRL.high;
+    const euro = data.EURBRL.high;
+    const bitCoin = data.BTCBRL.high;
+
+    setRealValue(new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(inputValue)));
+
+    if (selectedCurrency === "US$ Dolar americano") {
+      setConvertedValue(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(Number(inputValue) / dolar));
+    }
+
+    if (selectedCurrency === "€ Euro") {
+      setConvertedValue(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(Number(inputValue) / euro));
+    }
+
+    if (selectedCurrency === "BitCoin") {
+      setConvertedValue(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'BTC' }).format(Number(inputValue) * bitCoin));
+    }
+  }
+
+  const changeCurrency = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSelectedCurrency(selectedValue);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-white min-h-screen flex justify-center items-center">
+      <div className="container p-4 bg-blue-900 shadow-lg rounded-lg">
+        <header className="bg-blue-600 p-2 rounded-t-lg flex justify-center items-center">
+          <h1 className="text-white text-lg font-semibold">Conversor de Moedas</h1>
+        </header>
+        <main className="p-4">
+          <label className="text-gray-400">Converter <b>de</b></label>
+          <select className="block bg-white border border-gray-300 rounded p-1 w-full text-black">
+            <option>$ Real Brasileiro</option>
+          </select>
+          <label className="text-gray-400">Converter <b>para</b></label>
+          <select className="block bg-white border border-gray-300 rounded p-1 w-full text-black" value={selectedCurrency} onChange={changeCurrency}>
+            {currencies.map(currency => (
+              <option key={currency.name}>{currency.name}</option>
+            ))}
+          </select>
+          <label className="text-gray-400"><strong>Valor</strong></label>
+          <input type="number" className="block bg-white border border-gray-300 rounded p-1 w-full text-black" placeholder="R$ 10.000,00" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+
+          <section className="border border-blue-600 rounded p-4 mt-4 flex flex-col items-center">
+            <div className="container-currency flex flex-col items-center">
+              <img src="/assets/brasil.png" alt="icone do Brasil" />
+              <p className="text-gray-400 font-semibold">Real</p>
+              <p className="text-blue-600 font-bold text-xl">{realValue}</p>
+            </div>
+            <div>
+              <img className="arrow-img" src="/convert/assests/Vector.png" />
+            </div>
+            <div className="container-currency flex flex-col items-center">
+            <img src={currencies.find(currency => currency.name === selectedCurrency)?.imgSrc} alt={`icone de ${selectedCurrency}`} />
+              <p className="text-gray-400 font-semibold">{selectedCurrency}</p>
+              <p className="text-blue-600 font-bold text-xl">{convertedValue}</p>
+            </div>
+          </section>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+export default Home;
